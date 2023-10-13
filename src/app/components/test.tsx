be1a3cfbda9f7f7e3e6f947142af38b81d2da9bb
acoderwannabe19 <acoderwannabe19>
2023-10-13 09:09:06 +0000

    First commit

---
+++ src/app/components/test.tsx
@@ -0,0 +1,12 @@
+// import { useState } from "react";
+
+// react component to say hello world ahen a button is clicked
+function HelloWorld() {
+    // const [showMessage, setShowMessage] = useState(false);
+    return (
+        <div>
+            {/* <button onClick={() => setShowMessage(!showMessage)}>Toggle</button> */}
+            <h1 className="text-lg">hoola</h1>
+        </div>
+        )}
+export default HelloWorld;
