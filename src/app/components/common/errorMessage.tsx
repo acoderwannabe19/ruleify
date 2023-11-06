937de4359350828253e1a5e973f7680a2f5cd916
Abk-coder <dkhadre@ept.sn>
2023-11-06 08:18:44 +0000

    adding message error component

--- src/app/components/common/errorMessage.tsx
+++ src/app/components/common/errorMessage.tsx
@@ -1,5 +1,5 @@
-export function errorMessage(message:"", ) {
+export function errorMessage({message}: {message: ""}) {
     return (
-        ``
+        <span style={{color: "red"}}>`Veuillez choisir {message}.`</span>
     )
 }
